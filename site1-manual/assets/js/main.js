dfe906ccb08fd43f67f8a33cee24283945d2d2a3
mla <code@mlang.co>
2021-07-07 01:39:50 +0200

    chore: add images, remove unnecessary fonts, add text

--- site1-manual/assets/js/main.js
+++ site1-manual/assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+//Dimension by html5up.net | @ajlkn under CCA 3.0 license https://creativecommons.org/licenses/by/3.0/
 
 (function($) {
 
@@ -15,14 +11,7 @@
 		$main_articles = $main.children('article');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
@@ -31,27 +20,6 @@
 			}, 100);
 		});
 
-	// Fix: Flexbox min-height bug on IE.
-		if (browser.name == 'ie') {
-
-			var flexboxFixTimeoutId;
-
-			$window.on('resize.flexbox-fix', function() {
-
-				clearTimeout(flexboxFixTimeoutId);
-
-				flexboxFixTimeoutId = setTimeout(function() {
-
-					if ($wrapper.prop('scrollHeight') > $window.height())
-						$wrapper.css('height', 'auto');
-					else
-						$wrapper.css('height', '100vh');
-
-				}, 250);
-
-			}).triggerHandler('resize.flexbox-fix');
-
-		}
 
 	// Nav.
 		var $nav = $header.children('nav'),
