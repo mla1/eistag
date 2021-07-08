d5a5826dbb8c34ca563a88b697d92bbe5d8699c9
mla <code@mlang.co>
2021-07-08 17:48:19 +0200

    chore: rescaled images, added build script

---
+++ bin/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npx html-minifier-terser --input-dir src \
+    --output-dir dist \
+    --file-ext html \
+    --collapse-whitespace \
+    --remove-comments \
+    --remove-redundant-attributes \
+    --remove-script-type-attributes \
+    --remove-tag-whitespace \
+    --use-short-doctype
+
+npx csso src/assets/css/main.css --output dist/assets/css/main.css
+npx csso src/assets/css/noscript.css --output dist/assets/css/noscript.css
+
+npx uglifyjs src/assets/js/main.js --compress -o dist/assets/js/main.js
+npx uglifyjs src/assets/js/util.js --compress -o dist/assets/js/util.js
